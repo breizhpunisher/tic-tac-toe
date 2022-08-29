6cf1c98f92a6fa9866bb9be5324405fff195715d
patrice <potache53@hotmail.fr>
2022-08-29 22:15:12 +0200

    version beta

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
 <template>
 <h1>{{ msg }}</h1>
   <div class="hello">
-    <div class="joeurs" v-if="tab==false">
+    <div class="joueurs" v-if="tab==false">
    <div class="joueur_1" >
     joueur 1 
     <label for="pseudo">choisissez votre pseudo:</label>
@@ -10,7 +10,7 @@
      <p>
        <label for="symbole">choisissez votre symbole:</label><br />
 
-       <select v-model="s1">
+       <select v-model="this.joueurs[1]">
            <option v-for="(symbole1) of selectionp1" :value="symbole1" :key="symbole1">{{symbole1}}</option>        
        </select>
    </p>
@@ -23,18 +23,19 @@
      <p>
        <label for="symbole">choisissez votre symbole:</label><br />
 
-       <select v-model="s2">
+       <select v-model="this.joueurs[0]">
         
            <option v-for="(symbole2, index) of selectionp2" :value="symbole2" :key="index">{{symbole2}}</option>  
        </select>
    </p>
    </div>
-   <p> {{s1}} VS {{s2}}</p>
+   <p> {{this.joueurs[1]}} VS {{this.joueurs[0]}}</p>
    </div>
    
    <div class="board" v-if= "tab!=false">
    <div class="case" v-for="i in 9" :data-index="i" :key="i" v-on:click="gestionClicCase($event)"></div>
 </div>
+<p> {{this.joueurs[1]}} VS {{this.joueurs[0]}}</p>
 </div>
 
 
@@ -53,19 +54,21 @@ export default {
   },
   data() {
     return {
+      tourJeu: 1,
       tab: false,
       s1: undefined,
       s2: undefined,
-      symboles: ['','♔', '♕', '♖', '♗', '♘', '♙']
-       
+      symboles: ['','♔', '♕', '♖', '♗', '♘', '♙'],
+      index0:1,
+      joueurs: [[],[]],
     }
   },
  computed: {
  
     selectionp1(){
       let v = [...this.symboles]
-      if(this.s2){
-        let s2 = this.s2
+      if(this.joueurs[1]){
+        let s2 = this.joueurs[0]
         v.splice(v.findIndex((elt) => elt ==s2), 1)
       }
      return v
@@ -73,8 +76,8 @@ export default {
     
     selectionp2(){
       let t = [...this.symboles]
-      if(this.s1){
-        let s1 = this.s1
+      if(this.joueurs[0]){
+        let s1 = this.joueurs[1]
         t.splice(t.findIndex((elt) => elt ==s1), 1)
       } 
       
@@ -83,16 +86,24 @@ export default {
     
   },
   methods: {
-gestionClicCase(event){
-  this.s1 = '♔'
-  event.target.innerHTML=this.s1
+gestionClicCase(event){ 
+  let val= this.tourJeu %2 
+  event.target.innerHTML=this.joueurs[val];
+  this.tourJeu= this.tourJeu + 1;
+
 },
+
 gestionChgtPage(){
 let tableau = [];
-  for (let index = 1; index <= 100; index++) if (tableau.filter((index) => index % 2 != 0)) 
-  {
-    this.tab = true;}else{this.tab = false;}
-   console.log(tableau.index) 
+  for (let index = 1; index <= 100; index++)
+  tableau[index]=index;
+
+   if (tableau [this.index0] % 2 != 0)
+  { this.tab = true;}
+  else
+  {this.tab = false;}
+   console.log(tableau[this.index0]) 
+   this.index0=this.index0+1;
 }
 
   }
@@ -109,9 +120,7 @@ let tableau = [];
 display: flex;
   width:100vw;
 }
-.joueur_1{
-  justify-content: center;
-}  
+
  .board{
     display: grid;
     grid-template-columns: repeat(3, 1fr);
@@ -120,10 +129,12 @@ display: flex;
     border: 1px solid black;  
     }
     .case{ 
-      margin-top: auto;
+      display: flex;
+      align-items: center;
+      justify-content: center;
       border: 1px solid black;
       height: 90px;    
-       background-color: rgb(111, 198, 134);
+      background-color: rgb(111, 198, 134);
     }
     .case:hover{
       background-color: rgb(55, 198, 100);
